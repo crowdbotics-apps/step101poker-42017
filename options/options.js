eedcbcdfdc4a9fd001e0ce4b6352e32f7d9c0802
Crowdbotics <team@crowdbotics.com>
2023-07-09 03:25:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "step101poker-42017",
+  url: "https://step101poker-42017.botics.co",
+  api: "https://step101poker-42017.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
